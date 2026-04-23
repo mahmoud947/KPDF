3629db546745193eb970bac0b49c82b355b603ed
Mahmoud <mahmoudkamal4673@gmail.com>
2026-04-23 12:36:53 +0200

    Add KPdfViewer and use rememberPdfViewerState in App composable

--- composeApp/src/commonMain/kotlin/com/mahmoud/kpdf/App.kt
+++ composeApp/src/commonMain/kotlin/com/mahmoud/kpdf/App.kt
@@ -1,8 +1,12 @@
 package com.mahmoud.kpdf
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.mahmoud.kpdf_compose.KPdfViewer
+import com.mahmoud.kpdf_compose.rememberPdfViewerState
 import com.mahmoud.kpdf_core.api.KPdfSource
 
 @Composable
@@ -12,6 +16,14 @@ fun App() {
        val source = KPdfSource.Url(
             url =  "https://ontheline.trincoll.edu/images/bookdown/sample-local-pdf.pdf",
             headers = emptyMap(),
+        )
+        val kPdfState = rememberPdfViewerState(
+            source = source,
+        )
+
+        KPdfViewer(
+            state = kPdfState,
+            modifier = Modifier.fillMaxSize()
         )
     }
 }
